6d6a0509c24597def63debb3f92fd0b15555925e
Kolosov Egor <e.kolosov@g.nsu.ru>
2025-10-08 13:15:56 +0700

    2

---
+++ kolosov/secondTask.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(void) {
+    // Задаём таймзону Калифорнии.
+    // Лучше IANA-имя (America/Los_Angeles), чем "PST8PDT".
+    if (setenv("TZ", "America/Los_Angeles", 1) != 0) {
+        // если setenv недоступен/неудачен, можно так:
+        putenv("TZ=America/Los_Angeles");
+    }
+    tzset();
+
+    time_t now = time(NULL);
+    struct tm *pt = localtime(&now);
+    if (!pt) return 1;
+
+    char buf[64];
+    // Пример формата: 2025-10-07 12:34:56 PDT
+    if (strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S %Z", pt) == 0) return 1;
+
+    printf("California time: %s\n", buf);
+    return 0;
+}
