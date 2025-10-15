5bd9621b9f0531c2fc29d24c23ebbee1389de48b
dext01 <130887490+dext01@users.noreply.github.com>
2025-10-15 17:46:54 +0700

    Update work10.c

--- senkov/work10.c
+++ senkov/work10.c
@@ -2,16 +2,25 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <wait.h>
+#include <sys/wait.h>
 
-int main(int argc, char *argv[ ]){
+int main(int argc, char *argv[]){
     int status;
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <program> [args...]\n", argv[0]);
+        return 1;
+    }
     if (fork() == 0) {
         execvp(argv[1], &argv[1]);
         perror(argv[1]);
         exit(127);
     }
     wait(&status);
-    printf("exit status: %d\n", WEXITSTATUS(status));
-     exit(0);
+    if (WIFEXITED(status))
+        printf("exit status: %d\n", WEXITSTATUS(status));
+    else if (WIFSIGNALED(status))
+        printf("killed by signal: %d\n", WTERMSIG(status));
+    else
+        printf("child status: 0x%x\n", status);
+    return 0;
 }
